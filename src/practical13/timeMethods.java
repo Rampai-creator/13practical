559d2671ba50173a02a6297ae7bf6f36126df278
Rampai-creator <4343349@myuwc.ac.za>
2026-02-25 14:50:04 +0200

    Main method created

---
+++ src/practical13/timeMethods.java
@@ -0,0 +1,23 @@
+package practical13;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+public class timeMethods {
+    public static int N = 32654; 
+    public static Node[] records = new Node[N + 1];
+    public static Node[] sortedRecords; 
+
+    public static void main(String args[]) {
+        DecimalFormat twoD = new DecimalFormat("0.00");
+        DecimalFormat fourD = new DecimalFormat("0.0000");
+        DecimalFormat fiveD = new DecimalFormat("0.00000");
+
+        long start, finish;
+        double runTime = 0, runTime2 = 0, time;
+        double totalTime = 0.0;
+        int n = 30; //Number of keys to search 
+        int repetition, repetitions = 30;
+
+        
